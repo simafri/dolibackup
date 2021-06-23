f197a97731b768d7803bc39093fa17c0e361e0a7
Ando <ando.h@atoutpersona.com>
2021-06-23 16:21:14 +0300

    Use SESSION variable instead of the function newToken()

--- admin/admin.php
+++ admin/admin.php
@@ -110,7 +110,7 @@ print '<br>';
 
 
 print '<form action="'.$_SERVER["PHP_SELF"].'" method="post">';
-print '<input type="hidden" name="token" value="'.newToken().'">';
+print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
 print '<input type="hidden" name="action" value="set">';
 print '<table class="noborder centpercent">';
 print '<tr class="liste_titre">';
